2484e939212e72ad578c7e783b8c44b8363e61c9
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-02-09 17:57:39 +0000

    some css stff, logo, etc

--- trunk/application/forms/Search.php
+++ trunk/application/forms/Search.php
@@ -8,16 +8,17 @@ class Form_Search extends Zend_Form {
         public function init() {
                 // set the method for the display form to POST
                 $this->setMethod ( 'get' );
-
-               // $this->setAction('/search/files/q/', $_POST['q']);
+                
+               //$this->setAction('/search/index');
                 // add an email element
                 $this->addElement ( 'text', 'q', array (
                     'required' => false,
-                    'filters' => array ('StringTrim' ),
-                    //'validators' => array ('EmailAddress' )
+                    'filters' => array ('StringTrim' )
                      ) );
 
-
+                    $this->removeDecorator('HtmlTag');
+                    $this->removeDecorator('DtDdWrapper');
+                   // $this->groupname->removeDecorator('DtDdWrapper');
                 // add the submit button
                 $this->addElement ( 'submit', 'submit', array ('label' => 'Search' ) );
         }
