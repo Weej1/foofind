e7b1c912a86d1f9a6759a4309b8b7fed497a40d5
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-02-15 15:12:24 +0000

    added titles to echach section

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,9 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
+
+
+
         $this->_flashMessenger = $this->_helper->getHelper ( 'FlashMessenger' );
         $this->view->mensajes = $this->_flashMessenger->getMessages ();
     }
