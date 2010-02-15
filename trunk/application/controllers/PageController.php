0e059edbd849dbe12555f5543b0b283b94f9494d
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-02-15 11:17:22 +0000

    some changes on forms and css, removed complaint controller

--- trunk/application/controllers/PageController.php
+++ trunk/application/controllers/PageController.php
@@ -9,6 +9,11 @@ class PageController extends Zend_Controller_Action
 
         public function init(){
             $this->_helper->layout()->setLayout('page');
+
+            $this->_flashMessenger = $this->_helper->getHelper ( 'FlashMessenger' );
+            $this->view->mensajes = $this->_flashMessenger->getMessages ();
+
+
         }
 
         /*default action */
@@ -43,10 +48,66 @@ class PageController extends Zend_Controller_Action
 
        }
 
+        public function complaintAction() {
+                $request = $this->getRequest ();
+                $form = $this->_getComplaintForm ();
+
+
+                // check to see if this action has been POST'ed to
+                if ($this->getRequest ()->isPost ()) {
+
+                        // now check to see if the form submitted exists, and
+                        // if the values passed in are valid for this form
+                        if ($form->isValid ( $request->getPost () )) {
+
+                                // collect the data from the user
+                                $f = new Zend_Filter_StripTags ( );
+                                $email = $f->filter ( $this->_request->getPost ( 'email' ) );
+                                $message = $f->filter ( $this->_request->getPost ( 'message' ) );
+
+                                //get the username if its nolotiro user
+                                $user_info = $this->view->user->username;
+                                $user_info .= $_SERVER ['REMOTE_ADDR'];
+                                $user_info .= ' ' . $_SERVER ['HTTP_USER_AGENT'];
+
+                                $mail = new Zend_Mail ('utf-8');
+                                $body = $user_info.'<br/>'.$message;
+                                $mail->setBodyHtml ( $body );
+                                $mail->setFrom ( $email );
+                                $mail->addTo ( 'daniel@mp2p.net', 'Daniel Remeseiro' );
+                                $mail->setSubject ( 'foofind.com - complaint  from ' . $email );
+
+                                try {
+                                      $mail->send();
+                                    } catch (Exception $e) {
+                                      echo "Failed to Send Email.";
+                                    }
+
+
+                                $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Message sent successfully!' ) );
+                                $this->_redirect ( '/' );
+
+                        }
+                }
+                // assign the form to the view
+                $this->view->form = $form;
+
+        }
+
+        /**
+         *
+         * @return Form_Complaint
+         */
+        protected function _getComplaintForm() {
+                require_once APPLICATION_PATH . '/forms/Complaint.php';
+                $form = new Form_Complaint ( );
+
+                return $form;
+        }
 
        public function contactAction(){
 
-            $request = $this->getRequest ();
+                $request = $this->getRequest ();
                 $form = $this->_getContactForm();
 
 
@@ -73,7 +134,11 @@ class PageController extends Zend_Controller_Action
                                 $mail->setFrom ( $email );
                                 $mail->addTo ( 'daniel@mp2p.net', 'Daniel Remeseiro' );
                                 $mail->setSubject ( 'foofind.com - complaint  from ' . $email );
-                                $mail->send ();
+                                 try {
+                                      $mail->send();
+                                    } catch (Exception $e) {
+                                      echo "Failed to Send Email.";
+                                    }
 
                                 $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Message sent successfully!' ) );
                                 $this->_redirect ( '/' );
