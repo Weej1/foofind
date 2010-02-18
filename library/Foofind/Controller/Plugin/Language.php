fd2e74f4f58e93451a89d71259ab7d84b0ada435
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-02-18 18:19:15 +0000

    added new plugin language (i18n)

---
+++ library/Foofind/Controller/Plugin/Language.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Front Controller Plugin
+ *
+ * @uses	   Zend_Controller_Plugin_Abstract
+ * @subpackage Plugins
+ */
+class Foofind_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract {
+
+
+	public function routeShutdown(Zend_Controller_Request_Abstract $request) {
+		$locale = new Zend_Locale ( );
+		$options = array ('scan' => Zend_Translate::LOCALE_FILENAME );
+		$translate = new Zend_Translate ( 'csv', FOOFIND_PATH . '/application/lang/', 'auto', $options );
+		$requestParams = $this->getRequest ()->getParams ();
+		$language = (isset ( $requestParams ['language'] )) ? $requestParams ['language'] : false;
+		if ($language == false) {
+			$language = ($translate->isAvailable ( $locale->getLanguage () )) ? $locale->getLanguage () : 'es';
+		}
+		if (! $translate->isAvailable ( $language )) {
+
+			throw new Zend_Controller_Action_Exception ( 'This language is not available (yet)', 404 );
+		} else {
+			$locale->setLocale ( $language );
+			$translate->setLocale ( $locale );
+			Zend_Form::setDefaultTranslator ( $translate );
+			setcookie ( 'lang', $locale->getLanguage (), null, '/' );
+			Zend_Registry::set ( 'Zend_Locale', $locale );
+			Zend_Registry::set ( 'Zend_Translate', $translate );
+
+		}
+
+	}
+
+}
