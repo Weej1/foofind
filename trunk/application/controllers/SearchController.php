f9b0a67b030a5e11b126e202f25cf05c899208bd
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-03-29 18:59:50 +0000

    search input user filters improved

--- trunk/application/controllers/SearchController.php
+++ trunk/application/controllers/SearchController.php
@@ -362,18 +362,16 @@ class SearchController extends Zend_Controller_Action {
 
     public function init() {
 
-        $request = $this->getRequest ();
-
-        $requesttitle .= ' '.$this->_xss_clean($this->_getParam('q'));
-        $this->view->headTitle()->append(' - ');
-        $this->view->headTitle()->append($requesttitle);
+        //
     }
 
 
     public function indexAction() {
 
 
-        $qw = $this->_getParam('q');
+
+
+        $qw = stripcslashes(strip_tags($this->_getParam('q')));
         $type = $this->_getParam('type');
         $page = $this->_getParam('page', 1);
         $src = $this->_getParam('src');
@@ -390,9 +388,9 @@ class SearchController extends Zend_Controller_Action {
         $f = new Zend_Filter();
         $f->addFilter(new Zend_Filter_HtmlEntities($encoding));
         $f->addFilter(new Zend_Filter_StringTrim());
-        $f->addFilter(new Zend_Filter_StripTags($encoding));
-       
-        
+        //$f->addFilter(new Zend_Filter_StripTags($encoding));
+
+
 
         $q = $f->filter ( $qw );
         $type = $f->filter ( $type );
@@ -403,11 +401,16 @@ class SearchController extends Zend_Controller_Action {
         $brate = $f->filter ( $brate );
 
 
+       
+        $this->view->headTitle()->append(' - ');
+        $this->view->headTitle()->append($qw);
+
+
 
         $form = $this->_getSearchForm();
 
 
- if (!$q) { // check if query search is empty
+         if (!$q) { // check if query search is empty
 
             $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Hey! Write something' ) );
             $this->_redirect ( '/' );
@@ -416,7 +419,7 @@ class SearchController extends Zend_Controller_Action {
 
 
 
-        $form->getElement('q')->setValue(trim($qw));
+        $form->getElement('q')->setValue(trim($q));
 
         $form->loadDefaultDecoratorsIsDisabled(false);
 
@@ -448,7 +451,7 @@ class SearchController extends Zend_Controller_Action {
         $this->view->src = $srcs;
 
 
-        $SphinxPaginator = new Sphinx_Paginator('idx_files',array('query'=>$qw, 'src'=>$src2, 'type'=>$type, 'size' => $size, 'year' => $year, 'brate' => $brate));
+        $SphinxPaginator = new Sphinx_Paginator('idx_files',array('query'=>$q, 'src'=>$src2, 'type'=>$type, 'size' => $size, 'year' => $year, 'brate' => $brate));
 
         if ($SphinxPaginator !== null) {
                 //paginator
@@ -509,46 +512,6 @@ class SearchController extends Zend_Controller_Action {
                 return $form;
         }
 
-
-
-        protected function _xss_clean($data)
-        {
-        // Fix &entity\n;
-        $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
-        $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
-        $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
-        //$data = html_entity_decode($data);
-
-
-        $data = strip_tags($data);
-        $data = htmlentities($data, ENT_QUOTES, 'UTF-8');
-        // Remove any attribute starting with "on" or xmlns
-        $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
-
-        // Remove javascript: and vbscript: protocols
-        $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
-        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
-        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
-
-        // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
-        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
-
-        // Remove namespaced elements (we do not need them)
-        $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
-
-        do
-        {
-                // Remove really unwanted tags
-                $old_data = $data;
-                $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
-        }
-        while ($old_data !== $data);
-
-        // we are done...
-        return $data;
-        }
 
 
 
