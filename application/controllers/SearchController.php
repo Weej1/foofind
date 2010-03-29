0e46334be78e993675dd614caff193771ad0b6ec
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-03-29 10:54:04 +0000

    changed input filters

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -380,31 +380,29 @@ class SearchController extends Zend_Controller_Action {
         $year = $this->_getParam('year');
         $brate = $this->_getParam('brate');
 
+        
+        
+        $form = $this->_getSearchForm();
 
 
-        if (!$q) { // check if query search is empty
+        $q = $this->_xss_clean($q);
+        $type = $this->_xss_clean( $type );
+        $src = $this->_xss_clean ( $src );
+        $size = $this->_xss_clean ( $size );
+        $opt = $this->_xss_clean( $opt );
+        $year = $this->_xss_clean ( $year );
+        $brate = $this->_xss_clean ( $brate );
+
+
+
+ if (!$q) { // check if query search is empty
 
             $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Hey! Write something' ) );
             $this->_redirect ( '/' );
             return ;
         }
-        
-        $form = $this->_getSearchForm();
-       
 
-        // Create a filter chain and add filters
-        $f = new Zend_Filter();
-        $f->addFilter(new Zend_Filter_StripTags());
-        
-        $q = $f->filter ( $q );
-        $type = $f->filter ( $type );
-        $src = $f->filter ( $src );
-        $size = $f->filter ( $size );
 
-        $opt = $f->filter ( $opt );
-
-        $year = $f->filter ( $year );
-        $brate = $f->filter ( $brate );
 
         $form->getElement('q')->setValue(trim($q));
 
@@ -498,6 +496,46 @@ class SearchController extends Zend_Controller_Action {
 
                 return $form;
         }
+
+
+
+        protected function _xss_clean($data)
+        {
+        // Fix &entity\n;
+        $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+        $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+        $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+        $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+
+        // Remove any attribute starting with "on" or xmlns
+        $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+
+        // Remove javascript: and vbscript: protocols
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+        // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+        // Remove namespaced elements (we do not need them)
+        $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+        do
+        {
+                // Remove really unwanted tags
+                $old_data = $data;
+                $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+        }
+        while ($old_data !== $data);
+
+        // we are done...
+        return $data;
+        }
+
+
 
 }
 
