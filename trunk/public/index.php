865a83e5ff87314170173140422e33d2fa63a7e7
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-03-29 10:11:53 +0000

    opened index

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-//include_once 'sas.php';
-
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
