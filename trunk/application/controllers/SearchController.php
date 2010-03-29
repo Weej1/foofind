93ce978b0b3f13c35e630e4ef0363bd00d4b8a95
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-03-29 15:29:47 +0000

    fixed filtering with utf characters like ñ

--- trunk/application/controllers/SearchController.php
+++ trunk/application/controllers/SearchController.php
@@ -442,13 +442,13 @@ class SearchController extends Zend_Controller_Action {
 
         require_once APPLICATION_PATH.'/views/helpers/QueryString_View_Helper.php';
         $helper = new QueryString_View_Helper();
-        $helper->setParams(array('q'=>trim($q), 'type'=>$type, 'page'=>$page, 'src'=>$src2, 'opt'=>$opt, 'size' => $size, 'year' => $year, 'brate' => $brate));
+        $helper->setParams(array('q'=>trim($qw), 'type'=>$type, 'page'=>$page, 'src'=>$src2, 'opt'=>$opt, 'size' => $size, 'year' => $year, 'brate' => $brate));
         
         $this->view->registerHelper($helper, 'qs');
         $this->view->src = $srcs;
 
 
-        $SphinxPaginator = new Sphinx_Paginator('idx_files',array('query'=>$q, 'src'=>$src2, 'type'=>$type, 'size' => $size, 'year' => $year, 'brate' => $brate));
+        $SphinxPaginator = new Sphinx_Paginator('idx_files',array('query'=>$qw, 'src'=>$src2, 'type'=>$type, 'size' => $size, 'year' => $year, 'brate' => $brate));
 
         if ($SphinxPaginator !== null) {
                 //paginator
