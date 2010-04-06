c459a62c919e0f0acab56790c7944e8a847a024a
leiodd <leiodd@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-04-06 09:01:50 +0000

    Cache db metadata.

--- trunk/application/models/Files.php
+++ trunk/application/models/Files.php
@@ -1,4 +1,11 @@
 <?php
+// First, set up the Cache
+$cache = Zend_Cache::factory('Core', 'File',
+                             array('automatic_serialization' => true),
+                            array('cache_dir' => '/tmp'));
+
+// Next, set the cache to be used with all table objects
+Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
 
 class ff_file extends Zend_Db_Table
 {
