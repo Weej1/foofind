9481f11ab6e6926d0143d7f3db952d594af3c76d
daniel.remeseiro <daniel.remeseiro@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-05-10 09:35:30 +0000

    added ZFDebug plugin in dev and staging

--- trunk/application/Bootstrap.php
+++ trunk/application/Bootstrap.php
@@ -22,7 +22,44 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         return $moduleLoader;
     }
 
+    protected function _initZFDebug()
+{
 
+    if (APPLICATION_ENV!='production'){
+    $autoloader = Zend_Loader_Autoloader::getInstance();
+    $autoloader->registerNamespace('ZFDebug');
+
+    $options = array(
+        'plugins' => array('Variables',
+                           'File' => array('base_path' => APPLICATION_PATH),
+                           'Memory',
+                           'Time',
+                           'Registry',
+                           'Exception')
+    );
+
+    # Instantiate the database adapter and setup the plugin.
+    # Alternatively just add the plugin like above and rely on the autodiscovery feature.
+    if ($this->hasPluginResource('db')) {
+        $this->bootstrap('db');
+        $db = $this->getPluginResource('db')->getDbAdapter();
+        $options['plugins']['Database']['adapter'] = $db;
+    }
+
+    # Setup the cache plugin
+    if ($this->hasPluginResource('cache')) {
+        $this->bootstrap('cache');
+        $cache = $this-getPluginResource('cache')->getDbAdapter();
+        $options['plugins']['Cache']['backend'] = $cache->getBackend();
+    }
+
+    $debug = new ZFDebug_Controller_Plugin_Debug($options);
+
+    $this->bootstrap('frontController');
+    $frontController = $this->getResource('frontController');
+    $frontController->registerPlugin($debug);
+    }
+}
 
     protected function _initPlugins()
     {
