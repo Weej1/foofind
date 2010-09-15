3adfb3430ec7e73f30ab98eb367a0e6832e36de8
fernando.calo.sanchez@gmail.com <fernando.calo.sanchez@gmail.com@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2010-09-15 16:23:31 +0000

    Página para obtener el hexadecimal de una Url y viceversa.

---
+++ application/controllers/UrlCalcController.php
@@ -0,0 +1,35 @@
+<?php
+
+class UrlCalcController extends Zend_Controller_Action {
+
+    public function init() {
+         //validate domain foofind
+        $this->_helper->checkdomain->check();
+        
+    }
+
+    public function indexAction() {
+        $this->view->lang = $this->_helper->checklang->check();
+
+        $auth = Zend_Auth::getInstance();
+        if (!$auth->hasIdentity()) $this->_redirect("/");
+        if ( $auth->getIdentity()->type != 1 ) $this->_redirect("/");
+
+        require_once APPLICATION_PATH.'/controllers/helpers/Fileutils.php';
+        $helper = new Zend_Controller_Action_Helper_Fileutils();
+
+
+        if($this->_getParam('url')!=null){
+            $this->view->hex=$helper->uri2hex($helper->url2uri($this->_getParam('url')));
+            $this->view->url=$this->_getParam('url');
+        }
+        if($this->_getParam('hex')!=null){
+            $this->view->url=$helper->uri2url($helper->hex2uri($this->_getParam('hex')));
+            $this->view->hex=$this->_getParam('hex');
+        }
+
+
+    }
+
+}
+
