1871f057fec855ca0ef606f7bd188a4b7c77ba72
leiodd <leiodd@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2011-03-15 15:58:15 +0000

    Improved URL data handling on download page.

--- application/controllers/UrlCalcController.php
+++ application/controllers/UrlCalcController.php
@@ -18,11 +18,15 @@ class UrlCalcController extends Zend_Controller_Action {
         require_once APPLICATION_PATH.'/controllers/helpers/Fileutils.php';
         $helper = new Zend_Controller_Action_Helper_Fileutils();
 
-        if($this->_getParam('url')!=null){
+        if ($this->_getParam('src')!=null) {
+            $this->view->src=$this->_getParam('src');
+            $uri = substr(hash("sha256", $this->view->src), 0, 24);
+            $this->view->hex=$uri;
+            $this->view->url=$helper->uri2url($helper->hex2uri($uri));
+        } elseif($this->_getParam('url')!=null){
             $this->view->hex=$helper->uri2hex($helper->url2uri($this->_getParam('url')));
             $this->view->url=$this->_getParam('url');
-        }
-        if($this->_getParam('hex')!=null){
+        } elseif($this->_getParam('hex')!=null){
             $this->view->url=$helper->uri2url($helper->hex2uri($this->_getParam('hex')));
             $this->view->hex=$this->_getParam('hex');
         }
