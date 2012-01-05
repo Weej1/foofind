62a24e0f68e6f6de729ac36c54f36df9303f853c
leiodd <leiodd@0a674d9c-10d6-11df-b42a-7dbe0d947131>
2012-01-05 18:44:35 +0000

    Check links.

--- branches/mongo/application/controllers/PageController.php
+++ branches/mongo/application/controllers/PageController.php
@@ -235,99 +235,104 @@ class PageController extends Zend_Controller_Action
         public function complaintAction() {
                 $request = $this->getRequest ();
                 $form = $this->_getComplaintForm ();
-
+                $form->getElement("linkreported")->setValue("http://foofind.com/".$this->view->lang."/download/");
 
                 if ($this->getRequest ()->isPost ()) {
 
-                        
-                        if ($form->isValid ( $request->getPost () )) {
+                    $valid = $form->isValid ( $request->getPost () );
 
-                                //check agree tos and privacy
-                                $checkagree = ($this->_request->getPost ( 'agree' ) == '1');
-                                if ( $checkagree == FALSE  )
-                                {
-                                    $view = $this->initView();
-                                    $view->error .= $this->view->translate('Please, accept the terms of use and privacy policy');
-                    
-                                } else {
-
-                                // collect the data from the user
-                                $f = new Zend_Filter_StripTags ( );
-
-                                $name = $f->filter ( $this->_request->getPost ( 'name' ) );
-                                $surname = $f->filter ( $this->_request->getPost ( 'surname' ) );
-                                $company = $f->filter ( $this->_request->getPost ( 'company' ) );
-                                $email = $f->filter ( $this->_request->getPost ( 'email' ) );
-                                $phonenumber = $f->filter ( $this->_request->getPost ( 'phonenumber' ) );
-                                $linkreported = $f->filter ( $this->_request->getPost ( 'linkreported' ) );
-                                $urlreported = $f->filter ( $this->_request->getPost ( 'urlreported' ) );
-                                $reason = $f->filter ( $this->_request->getPost ( 'reason' ) );
-                                $message = $f->filter ( $this->_request->getPost ( 'message' ) );
-
+                    // collect the data from the user
+                    $f = new Zend_Filter_StripTags ( );
                                 
-                                $user_info = $_SERVER ['REMOTE_ADDR'];
-                                $user_info .= ' ' . $_SERVER ['HTTP_USER_AGENT'];
+                    $message = $f->filter ( $this->_request->getPost ( 'message' ) );
+                           
+                    if (preg_match("|https?://[^/]*foofind.com/?|i", $message)>0)
+                    {
+                        //check message don't include foofind links
+                        $m = $form->getElement("message")->addError("Please, include the reported links in the appropriate fields on the form.");
+                        $valid = false;
+                    }
+                    if ($valid) {
+                        $checkagree = ($this->_request->getPost ( 'agree' ) == '1');
+                        if ( $checkagree == FALSE  )
+                        {
+                            //check agree tos and privacy
+                            $view = $this->initView();
+                            $view->error .= $this->view->translate('Please, accept the terms of use and privacy policy');
+                            $valid = false;
 
-                                $mail = new Zend_Mail ('utf-8');         
-                                $body = $user_info
-                                .'<br/><br/><br/>Name: '.$name.' '.$surname
-                                .'<br/><br/>Company: '.$company
-                                .'<br/><br/>Phone number: '.$phonenumber
-                                .'<br/><br/>Link reported: '.$linkreported
-                                .'<br/><br/>Url reported: '.$urlreported
-                                .'<br/><br/>Reason: '.$reason
-                                .'<br/><br/>Message: '.$message;
-                                $mail->setBodyHtml ( $body );
-                                $mail->setFrom ( $email );
-
-                                $config = Zend_Registry::get('config');
-                                $mail->addTo($config->contact->email);
-                                $mail->setSubject ( 'foofind.com - complaint petition  from ' . $email );
-
-                                try {
-                                      $mail->send();
-                                    } catch (Exception $e) {
-
-                                     echo  'Failed to Send Email.';
-                                      $this->_redirect ( '/' );
-                                    }
-
-
-                                //now we send the autoreply  to the claimer
-                                $mailautoreply = new Zend_Mail('utf-8');
-
-                                $body2 =
-                                '<br/><br/>Esta es una respuesta automática a su solicitud de retirada de enlace.<br/><br/>
-Hemos recibido su solicitud y la procesaremos lo antes posible. No se requiere ninguna acción por su parte para completar el proceso.<br/><br/>
-
-Tenga en cuenta que sólo podemos retirar enlaces específicos, no palabras de búsqueda, y que no podemos evitar que terceros sitios sigan ofreciendo los enlaces retirados por nosotros. Si lo desea, puede comprobar la no disponibilidad de los enlaces que ha solicitado retirar visitando foofind.com dentro de unos días.
-<br/><br/>
-Atentamente,<br/>
-El equipo Foofind.
-';
-                                $mailautoreply->setBodyHtml ( $body2 );
-                                $mailautoreply->setFrom ( 'noreply@foofind.com' );
-                                $mailautoreply->addTo ( $email , $name.' '.$surname);
-                                $mailautoreply->setSubject ( 'foofind.com - about your complaint petition  , ' . $name.' '.$surname );
-
-                                try {
-                                      $mailautoreply->send();
-                                    } catch (Exception $e) {
-
-                                     echo  'Failed to Send Email.';
-                                      $this->_redirect ( '/' );
-                                    }
+                        } else {
+                            $name = $f->filter ( $this->_request->getPost ( 'name' ) );
+                            $surname = $f->filter ( $this->_request->getPost ( 'surname' ) );
+                            $company = $f->filter ( $this->_request->getPost ( 'company' ) );
+                            $email = $f->filter ( $this->_request->getPost ( 'email' ) );
+                            $phonenumber = $f->filter ( $this->_request->getPost ( 'phonenumber' ) );
+                            $linkreported = $f->filter ( $this->_request->getPost ( 'linkreported' ) );
+                            $urlreported = $f->filter ( $this->_request->getPost ( 'urlreported' ) );
+                            $reason = $f->filter ( $request->getPost ( 'reason' ) );
 
 
-                                //process finished
-                                $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Message sent successfully!' ) );
-                                $this->_redirect ( '/' );
+                            $user_info = $_SERVER ['REMOTE_ADDR'];
+                            $user_info .= ' ' . $_SERVER ['HTTP_USER_AGENT'];
 
+                            $mail = new Zend_Mail ('utf-8');
+                            $body = $user_info
+                            .'<br/><br/><br/>Name: '.$name.' '.$surname
+                            .'<br/><br/>Company: '.$company
+                            .'<br/><br/>Phone number: '.$phonenumber
+                            .'<br/><br/>Link reported: '.$linkreported
+                            .'<br/><br/>Url reported: '.$urlreported
+                            .'<br/><br/>Reason: '.$reason
+                            .'<br/><br/>Message: '.$message;
+                            $mail->setBodyHtml ( $body );
+                            $mail->setFrom ( $email );
+
+                            $config = Zend_Registry::get('config');
+                            $mail->addTo($config->contact->email);
+                            $mail->setSubject ( 'foofind.com - complaint petition  from ' . $email );
+
+                            try {
+                                  $mail->send();
+                                } catch (Exception $e) {
+
+                                 echo  'Failed to Send Email.';
+                                  $this->_redirect ( '/' );
+                                }
+
+
+                            //now we send the autoreply  to the claimer
+                            $mailautoreply = new Zend_Mail('utf-8');
+
+                            $body2 =
+                            '<br/><br/>Esta es una respuesta automática a su solicitud de retirada de enlace.<br/><br/>
+        Hemos recibido su solicitud y la procesaremos lo antes posible. No se requiere ninguna acción por su parte para completar el proceso.<br/><br/>
+
+        Tenga en cuenta que sólo podemos retirar enlaces específicos, no palabras de búsqueda, y que no podemos evitar que terceros sitios sigan ofreciendo los enlaces retirados por nosotros. Si lo desea, puede comprobar la no disponibilidad de los enlaces que ha solicitado retirar visitando foofind.com dentro de unos días.
+        <br/><br/>
+        Atentamente,<br/>
+        El equipo Foofind.
+        ';
+                            $mailautoreply->setBodyHtml ( $body2 );
+                            $mailautoreply->setFrom ( 'noreply@foofind.com' );
+                            $mailautoreply->addTo ( $email , $name.' '.$surname);
+                            $mailautoreply->setSubject ( 'foofind.com - about your complaint petition  , ' . $name.' '.$surname );
+
+                            try {
+                                  $mailautoreply->send();
+                                } catch (Exception $e) {
+
+                                 echo  'Failed to Send Email.';
+                                  $this->_redirect ( '/' );
+                                }
+
+
+                            //process finished
+                            $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Message sent successfully!' ) );
+                            $this->_redirect ( '/' );
                         }
-                        
-                        }
-
+                    }
                 }
+
                 // assign the form to the view
                 $this->view->form = $form;
 
